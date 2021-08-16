9387bdc9f03cfec2e839339cf82bd6221ec3fd86
Mikiya-I <mikiya19961012@gmail.com>
2021-08-16 23:25:48 +0900

    Changes

--- tesseractOCR/defTesstract.py
+++ tesseractOCR/defTesstract.py
@@ -13,11 +13,12 @@ tools = pyocr.get_available_tools()
 tool = tools[0]
 
 # OCR対象の画像ファイルを読み込む
-img = Image.open("tesseract-3.jpg")
+# img = Image.open("tesseract-3.jpg")
 img = Image.open("handWrittenSample.png")
 
 # 画像から文字を読み込む
-builder = pyocr.builders.TextBuilder(tesseract_layout=6)
+# 読み込み方の指定(ここでは数字+記号)
+builder = pyocr.builders.DigitBuilder(tesseract_layout=6)
 text = tool.image_to_string(img, lang="jpn", builder=builder)
 
 print(text)
